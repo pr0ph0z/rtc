3efdd3f89f19483999f6501de433cfd34bea6fba
Mohamad Radisha <pr0ph0z23@gmail.com>
2019-05-30 10:49:49 +0700

    Add eslint config

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const cors = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  next()
+}
