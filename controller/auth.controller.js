c6aec833379ad5b048053b599d5bd47a756f4239
Mohamad Radisha <pr0ph0z23@gmail.com>
2019-06-09 11:05:11 +0700

    Insert token

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,18 +1,40 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const services = require('../services/index')
 const utils = require('../utils')
+const uuidv1 = require('uuid/v1')
+const uuidv3 = require('uuid/v3')
 
 let response = null
 
 exports.register = async (req, res, next) => {
   if (utils.checkBody(req.body, utils.registerBody)) {
-    let User = services.db.collection('test')
+    let User = services.db.collection('users')
+    let Token = services.db.collection('tokens')
+
+    let uid = uuidv3(req.body.username, uuidv1())
+    let token = jwt.sign({
+      data: {
+        username: req.body.username,
+        uid: uid
+      }
+    }, process.env.SECRET_KEY)
+
     await User.insertOne({
+      uid: uid,
       username: req.body.username,
       password: req.body.password,
       email: req.body.email,
       profile: {
         full_name: req.body.full_name
-      }
+      },
+      token: token
+    })
+
+    await Token.insertOne({
+      uid: uid,
+      token: token,
+      created_at: new Date() / 1000 | 0
     })
     response = utils.requestResponse.success
   } else {
